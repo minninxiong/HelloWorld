8b576caba7852a90e85950280afe50b387068ddb
minninxiong <614104080@qq.com>
2017-12-23 21:13:29 +0800

    Create Hellow.java

---
+++ Hellow.java
@@ -0,0 +1,1 @@
+ Hellow{ private String name; public String getName(){ return this.name; } public void setName(String name){ this.name=name; } public void main(){ Hellow hellow = new Hellow(); hello.setName("limin"); system.out.print(hellow.getName()+":Hellow World!"); } }
